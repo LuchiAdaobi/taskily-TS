ad1d31cfccc96680e847b25236cc494b38ae628a
Luchi <luchichronicles@gmail.com>
2023-08-30 21:44:26 -0700

    initial commit

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,16 @@
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+export default function TodoList({ todos, setTodos }: Props) {
+  return <div className="todos">{
+    todos.map(todo => (
+        <SingleTodo todo={todo} key={todo.id}
+        todos={todos}
+        setTodos={setTodos}/>
+    ))
+  }</div>;
+}
